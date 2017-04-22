c4c2080f131957084acde83bad2dc752e6f4d2cc
Yuki Nagae <yuki.nagae1130@gmail.com>
2017-04-22 15:28:54 +1000

    first commit

---
+++ data_exploration.R
@@ -0,0 +1,53 @@
+library(tidytext)
+library(dplyr)
+library(stringr)
+library(wordcloud)
+
+library(ggplot2)
+
+path = "./data/"
+
+train = read.delim(paste0(path, "train.tsv"), header = FALSE, sep = " ")
+
+train_df = data_frame(word = train$V1, type = train$V2)
+
+train_df$word <- as.character(train_df$word)
+
+# NLP
+# see: https://github.com/statsmaths/coreNLP
+devtools::install_github("statsmaths/coreNLP")
+coreNLP::downloadCoreNLP()
+library(coreNLP)
+initCoreNLP()
+
+output = annotateString(train_df$word)
+getToken(output)[,c(1:3,7:8)]
+
+#
+
+train_df %>% mutate(tweet = cumsum(str_detect(word, regex("^BOS"))))
+
+train_df %>% count(type) %>% with(wordcloud(type, n, max.words = 100))
+
+train_df %>% count(type, sort=TRUE) %>%
+             mutate(type = reorder(type, n)) %>%
+             ggplot(aes(type, n)) + geom_col() + xlab(NULL) + coord_flip()
+
+#levels(train_df$word) <- tolower(levels(train_df$word))
+
+#filtered_train_df <- train_df %>%
+#                     filter(word != "bos") %>%
+#                     filter(word != "eos") %>%
+#                     filter(!str_detect(word, regex("^@"))) %>%
+#                     filter(!str_detect(word, regex("^http"))) %>%
+#                     anti_join(stop_words) %>%
+#                     filter(word != ".") %>%
+#                     filter(word != "...") %>%
+#                     filter(word != ",") %>%
+#                     filter(word != ":") %>%
+#                     filter(word != "?") %>%
+#                     filter(word != "!")
+
+#filtered_train_df %>% count(word, sort=TRUE)
+
+#filtered_train_df %>% count(word) %>% with(wordcloud(word, n, max.words = 100))
